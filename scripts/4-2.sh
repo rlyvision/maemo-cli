3ecd973772a8dc211ee3b559b2aff8a225888515
rlyvision <hamzaelasmar12@gmail.com>
2026-02-19 23:50:41 +0300

    Edited qemu script

--- scripts/4-2.sh
+++ scripts/4-2.sh
@@ -1,3 +1,10 @@
-echo "Downloading latest QEMU version."
-wget -O /tmp/maemo/maemo-leste-5.0-amd64-20251228.qcow2.xz https://maedevu.maemo.org/images/virtual-machines/20251228-daedalus/maemo-leste-5.0-amd64-20251228.qcow2.xz
-echo "Install done, copy from '/tmp/maemo/maemo-leste-5.0-amd64-20251228.qcow2.xz' to your target location.
+echo "Downloading latest QEMU image..."
+
+mkdir -p /tmp/maemo
+
+cd /tmp/maemo || exit 1
+
+wget https://maedevu.maemo.org/images/virtual-machines/20251228-daedalus/maemo-leste-5.0-amd64-20251228.qcow2.xz
+
+echo "Download complete."
+echo "File saved to: /tmp/maemo/maemo-leste-5.0-amd64-20251228.qcow2.xz"
