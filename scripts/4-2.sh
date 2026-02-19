efacfe3e9706dd8da3ee33acb605cef44a89481b
rlyvision <hamzaelasmar12@gmail.com>
2026-02-19 23:51:42 +0300

    Edited qemu script

--- scripts/4-2.sh
+++ scripts/4-2.sh
@@ -5,6 +5,6 @@ mkdir -p /tmp/maemo
 cd /tmp/maemo || exit 1
 
 wget https://maedevu.maemo.org/images/virtual-machines/20251228-daedalus/maemo-leste-5.0-amd64-20251228.qcow2.xz
-
+unxz maemo-leste-5.0-amd64-20251228.qcow2.xz
 echo "Download complete."
-echo "File saved to: /tmp/maemo/maemo-leste-5.0-amd64-20251228.qcow2.xz"
+echo "File saved to: /tmp/maemo/maemo-leste-5.0-amd64-20251228.qcow2"
